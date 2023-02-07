6b949cfeb4036de60a7862352d7bbbcf8558c449
saidjalolqodirov <saidjalolqodirov1@gmail.com>
2023-02-07 12:51:58 +0500

    command lin runner 1

--- src/main/java/uz/logistics/config/CommandLineRunnerImpl.java
+++ src/main/java/uz/logistics/config/CommandLineRunnerImpl.java
@@ -1,31 +1,23 @@
 package uz.logistics.config;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import uz.logistics.entity.Admin;
-import uz.logistics.repository.AdminRepository;
-
-import java.util.List;
-
 /**
  * @author: Saidjalol Qodirov 2/7/2023 12:34 PM
  */
-@Configuration
-@RequiredArgsConstructor
+//@Configuration
+//@RequiredArgsConstructor
 public class CommandLineRunnerImpl {
 
-    private final AdminRepository repository;
+//    private final AdminRepository repository;
 
-    @Bean
+    //    @Bean
     public void createAdmin() {
-        List<Admin> adminList = repository.findAll();
-        if (adminList.isEmpty()) {
-            Admin admin = new Admin();
-            admin.setId(1);
-            admin.setUsername("admin");
-            admin.setPassword("admin");
-            repository.save(admin);
-        }
+//        List<Admin> adminList = repository.findAll();
+//        if (adminList.isEmpty()) {
+//            Admin admin = new Admin();
+//            admin.setId(1);
+//            admin.setUsername("admin");
+//            admin.setPassword("admin");
+//            repository.save(admin);
+//        }
     }
 }
--- src/main/java/uz/logistics/entity/Admin.java
+++ src/main/java/uz/logistics/entity/Admin.java
@@ -61,6 +61,6 @@ public class Admin implements UserDetails {
 
     @Override
     public boolean isEnabled() {
-        return true ;
+        return true;
     }
 }
