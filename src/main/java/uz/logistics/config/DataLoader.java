f1084428c4e4ea87f9b5276f43223b49a2e8d306
saidjalolqodirov <saidjalolqodirov1@gmail.com>
2023-02-09 11:24:50 +0500

    sydtem config

--- src/main/java/uz/logistics/config/DataLoader.java
+++ src/main/java/uz/logistics/config/DataLoader.java
@@ -2,26 +2,32 @@ package uz.logistics.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import uz.logistics.entity.Admin;
 import uz.logistics.repository.AdminRepository;
+
+import java.util.List;
 
 /**
  * @author: Saidjalol Qodirov 2/4/2023 8:31 PM
  */
-//@Configuration
+@Configuration
 @RequiredArgsConstructor
 public class DataLoader implements CommandLineRunner {
 
     private final AdminRepository adminRepository;
 
+    private final PasswordEncoderI passwordEncoder;
+
     @Override
     public void run(String... args) throws Exception {
-//        List<Admin> adminList = adminRepository.findAll();
-//        if (adminList.isEmpty()) {
-//            Admin admin = new Admin();
-//            admin.setId(1);
-//            admin.setUsername("admin");
-//            admin.setPassword("admin");
-//            adminRepository.save(admin);
-//        }
+        List<Admin> adminList = adminRepository.findAll();
+        if (adminList.isEmpty()) {
+            Admin admin = new Admin();
+            admin.setId(1);
+            admin.setUsername("admin");
+            admin.setPassword(passwordEncoder.passwordEncoder().encode("admin"));
+            adminRepository.save(admin);
+        }
     }
 }
